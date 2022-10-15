3841201a14a3ce0b500079ac656bb3da335a3af0
dominoto <zubac76@gmail.com>
2022-10-15 18:40:12 +0200

    Combining commits using git reset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
+//import Particles from 'react-particles-js';
+import ParticlesBg from 'particles-bg';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
@@ -10,17 +11,22 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-const particlesOptions = {
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    }
-  }
-}
+// //tsparticles
+// import { useCallback } from "react";
+// import Particles from "react-tsparticles";
+// import { loadFull } from "tsparticles";
+
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 30,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     }
+//   }
+// }
 
 const initialState = {
   input: '',
@@ -118,11 +124,104 @@ class App extends Component {
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
+
+    // //tsparticles
+    // const particlesInit = useCallback(async (engine) => {
+    //   console.log(engine);
+    //   // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    //   // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    //   // starting from v2 you can add only the features you need reducing the bundle size
+    //   await loadFull(engine);
+    // }, []);
+
+    // const particlesLoaded = useCallback(async (container) => {
+    //   await console.log(container);
+    // }, []);
+
     return (
       <div className="App">
-        <Particles className='particles'
-          params={particlesOptions}
-        />
+        {/* <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fps_limit: 60,
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onclick: { enable: true, mode: "push" },
+                onhover: {
+                  enable: true,
+                  mode: "attract",
+                  parallax: { enable: false, force: 60, smooth: 10 }
+                },
+                resize: true
+              },
+              modes: {
+                push: { quantity: 4 },
+                attract: { distance: 200, duration: 0.4, factor: 5 }
+              }
+            },
+            particles: {
+              color: { value: "#ffffff" },
+              line_linked: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.4,
+                width: 1
+              },
+              move: {
+                attract: { enable: false, rotateX: 600, rotateY: 1200 },
+                bounce: false,
+                direction: "none",
+                enable: true,
+                out_mode: "out",
+                random: false,
+                speed: 2,
+                straight: false
+              },
+              number: { density: { enable: true, value_area: 800 }, value: 80 },
+              opacity: {
+                anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
+                random: false,
+                value: 0.5
+              },
+              shape: {
+                character: {
+                  fill: false,
+                  font: "Verdana",
+                  style: "",
+                  value: "*",
+                  weight: "400"
+                },
+                image: {
+                  height: 100,
+                  replace_color: true,
+                  src: "images/github.svg",
+                  width: 100
+                },
+                polygon: { nb_sides: 5 },
+                stroke: { color: "#000000", width: 0 },
+                type: "circle"
+              },
+              size: {
+                anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
+                random: true,
+                value: 5
+              }
+            },
+            polygon: {
+              draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
+              move: { radius: 10 },
+              scale: 1,
+              type: "none",
+              url: ""
+            },
+            retina_detect: true
+          }}
+        /> */}
+        <ParticlesBg type="fountain" bg={true} />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         {route === 'home'
           ? <div>
